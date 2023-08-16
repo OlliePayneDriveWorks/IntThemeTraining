eb3952e890ade63729ec253ed41297f1b6263625
OlliePayneDriveWorks <55238902+OlliePayneDriveWorks@users.noreply.github.com>
2023-08-16 12:42:25 +0100

    Added evnt listenr & render spec

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 const siteSettings = {
   dwEndpoint: "http://wsm-tech-olip",
   dwGroup: "Alias",
-  dwProject: "",
+  dwProject: "AllTheThings",
 };
 
 //Select the div to inset a spec into...
 const specForm = document.getElementById("specification-form");
 
 let dwAPI;
+let sessionId;
 
 //Inject the Integration theme JS into the webpage
 function injectClientLib() {
@@ -23,16 +24,33 @@ function injectClientLib() {
 //function to connect to DriveWorks and load the form
 async function loadDriveWorks() {
   try {
-    console.log(window);
-
+    //Declaring a new DW Client 'class' - this makes it easier to use the functions of the API
     dwAPI = new window.DriveWorksLiveClient(siteSettings.dwEndpoint);
 
+    // Logging into the Group
     const group = await dwAPI.loginGroup(siteSettings.dwGroup);
-    console.log(group);
 
-    console.log(dwAPI.sessionid);
+    // Storing the Session ID for use later
+    sessionId = group.sessionId;
+    console.log(sessionId);
+
+    //Create a new Specification with the settings from the top
+    const spec = await dwAPI.createSpecification(
+      siteSettings.dwGroup,
+      siteSettings.dwProject
+    );
+
+    // Render the spec in the <div> element
+    await spec.render(specForm);
   } catch (err) {
-    console.log(err);
+    // logging any caught errors to the log
+    console.error(err);
   }
 }
+
+document.getElementById("button-submit").addEventListener("click", function () {
+  //Add code to do things when the 'Submit' button is pressed.
+  console.log(`button pressed`);
+});
+
 injectClientLib();
